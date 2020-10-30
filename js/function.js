f75186f589af1a0c9ad383d2ad56d6a76f63703e
azebkidanemariam <azeb.kidanemariam2@gmail.com>
2020-10-30 11:10:55 +0100

    api

--- js/function.js
+++ js/function.js
@@ -1,10 +1,26 @@
 
-var starWarsPeopleList = document.querySelector("nav-list six-quarter");
+// var starWarsPeopleList = document.querySelector("nav-list six-quarter");
+
+// fetch('https://swapi.dev/api/people/') 
+// .then(function(response) {
+
+// 	 console.log(response)
+// })
+
+
+var starWarsPeopleList = document.querySelector('nav-list six-quarter');
 
 fetch('https://swapi.dev/api/people/') 
-.then(function(response) {
-
-	 console.log(response)
+.then(function(response) {   
+  return response.json(); 
 })
+.then(function(json) {
+  let people = json.results; 
 
+  for(p of people) {
+    let listItem = document.querySelector('nav-item'); 
+    listItem.innerHTML = '<p>' + p.name + '</p>'; 
+    starWarsPeopleList.appendChild(listItem);  
+  }
 
+});
