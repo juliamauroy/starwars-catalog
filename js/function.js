7715a96bd06bab347404973bc838d08d039f73e6
juliamauroy <julia.mauroy@iths.se>
2020-10-30 11:54:16 +0100

    Update function.js

--- js/function.js
+++ js/function.js
@@ -1,18 +1,30 @@
-let starWarsPeopleList = document.querySelector('nav-list six-quarter');
+// let starWarsPeopleList = document.querySelector('nav-list six-quarter');
 
-fetch('https://swapi.dev/api/people/) 
-.then(function(response) {
-  return response.json(); 
-})
-.then(function(json) {
-  let people = json.results; 
+// fetch("https://swapi.dev/api/people/") 
+// .then(function(response) {
+//   return response.json(); 
+// })
+// .then(function(json) {
+//   let people = json.results; 
 
-  for(p of people) {
-    let listItem = document.querySelector('nav-item'); 
-    listItem.innerHTML = '<p>' + p.name + '</p>'; 
-    starWarsPeopleList.appendChild(listItem);
-  }
+//   for(p of people) {
+//     let listItem = document.querySelector('nav-item'); 
+//     listItem.innerHTML = '<p>' + p.name + '</p>'; 
+//     starWarsPeopleList.appendChild(listItem);
+//   }
 
-});
+// })
 
-hej hej :)
+async function listCharacters(){
+	const response = await fetch("https://swapi.dev/api/people/")
+	const data = await response.json()
+	return data.results
+}
+
+async function renderCharacterList(){
+	const characters = await listCharacters()
+
+	for(let character of characters){
+		renderListItem(character)
+	}
+}
