661919af29a24fbe22c3d43dd86b684c2eb2493c
azebkidanemariam <azeb.kidanemariam2@gmail.com>
2020-10-30 13:47:40 +0100

    Update function.js

--- js/function.js
+++ js/function.js
@@ -1,35 +1,16 @@
-// let starWarsPeopleList = document.querySelector('nav-list six-quarter');
+ let starWarsPeopleList = document.querySelector('.nav-list.six-quarter');
 
-// fetch("https://swapi.dev/api/people/") 
-// .then(function(response) {
-//   return response.json(); 
-// })
-// .then(function(json) {
-//   let people = json.results; 
+fetch("https://swapi.dev/api/people/") 
+.then(function(response) {
+  return response.json(); 
+})
+.then(function(json) {
+  let people = json.results; 
 
-//   for(p of people) {
-//     let listItem = document.querySelector('nav-item'); 
-//     listItem.innerHTML = '<p>' + p.name + '</p>'; 
-//     starWarsPeopleList.appendChild(listItem);
-//   }
+  for(p of people) {
+    let listItem = document.createElement('li'); 
+    listItem.innerHTML = '<p>' + p.name + '</p>'; 
+    starWarsPeopleList.appendChild(listItem);
+  }
 
-// })
-
-async function listCharacters(){
-	const response = await fetch("https://swapi.dev/api/people/")
-	const data = await response.json()
-	return data.results
-}
-
-function renderListItems(character)
-let listItem = document.querySelectors(".box1")
-listItem.innerHtml = 
-document.querySelector(".mass").innerHTML = user.mass
-
-async function renderCharacterList(){
-	const characters = await listCharacters()
-
-	for(let character of characters){
-		renderListItem(character)
-	}
-}
+})
