33f8ec254f346cabceaa8ac7e641f8937e2d2039
azebkidanemariam <azeb.kidanemariam2@gmail.com>
2020-10-30 15:27:24 +0100

    api

--- js/function.js
+++ js/function.js
@@ -1,6 +1,5 @@
  let starWarsPeopleList = document.querySelector('.nav-list.six-quarter');
-
-fetch("https://swapi.dev/api/people/") 
+ fetch("https://swapi.dev/api/people/") 
 .then(function(response) {
   return response.json(); 
 })
@@ -8,9 +7,13 @@ fetch("https://swapi.dev/api/people/")
   let people = json.results; 
 
   for(p of people) {
-    let listItem = document.createElement('li'); 
-    listItem.innerHTML = '<li>' + p.name + '</li>'; 
-    starWarsPeopleList.appendChild(listItem);
-  }
+	let listItem = document.createElement('li');
+	 listItem.innerHTML = '<li>' + p.name + '</li>';
+	 starWarsPeopleList.appendChild(listItem);
+	 var list = document.querySelector(".nav-list.six-quarter");    // Get the <ul> element to insert a new node
+      list.insertBefore(listItem, list.childNodes[0])
 
+	 }
+  
+	
 })
