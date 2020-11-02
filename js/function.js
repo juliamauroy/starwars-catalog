f39c7d75d998c85685a8e60234ac867d056c7135
azebkidanemariam <azeb.kidanemariam2@gmail.com>
2020-11-02 15:43:48 +0100

    change

--- js/function.js
+++ js/function.js
@@ -1,19 +1,49 @@
- let starWarsPeopleList = document.querySelector('.nav-list.six-quarter');
- fetch("https://swapi.dev/api/people/") 
-.then(function(response) {
-  return response.json(); 
-})
-.then(function(json) {
-  let people = json.results; 
 
-  for(p of people) {
-	let listItem = document.createElement('li');
-	 listItem.innerHTML = '<li>' + p.name + '</li>';
-	 starWarsPeopleList.appendChild(listItem);
-	 var list = document.querySelector(".nav-list.six-quarter");    // Get the <ul> element to insert a new node
-      list.insertBefore(listItem, list.childNodes[0])
 
-	 }
+async function main() {
+    // const navList = document.querySelector('.nav-list');
+    let starWarsPeopleList = document.querySelector(".nav-list.six-quarter");
+    let currentPage = 1;
   
-	
-})
+    async function getPageData() {
+      const response = await fetch(
+        `https://swapi.dev/api/people/?page=${currentPage}`
+      );
+      return await response.json();
+    }
+    async function getCharacterInfo(url) {
+      const response = await fetch(url);
+      return await response.json();
+    }
+  
+    async function setCharacters() {
+      const characters = await getPageData();
+      for (character of characters.results) {
+        const li = document.createElement("li");
+        li.textContent = character.name;
+        li.dataset.url = character.url;
+        li.addEventListener("click", addCharacterInfo);
+        starWarsPeopleList.append(li);
+      }
+    }
+  
+    function addCharacterData(obj) {
+      const characterList = document.querySelector(".character-details__list");
+      characterList.innerHTML = `
+          <li>Name: ${obj.name}</li>
+          <li>Height: ${obj.height}</li>
+          <li>Mass: ${obj.mass}</li>
+          <li>Skin Color: ${obj.skin_color}</li>
+          `;
+    }
+  
+    async function addCharacterInfo(e) {
+      const character = await getCharacterInfo(e.target.dataset.url);
+      const homeWorld = await getCharacterInfo(character.homeworld);
+      addCharacterData(character);
+    }
+  
+    setCharacters();
+  }
+  
+  main();
